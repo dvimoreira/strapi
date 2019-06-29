e41288a1d1b5101247c75bc69e309876e26b812d
Alexandre Bodin <bodin.alex@gmail.com>
2019-06-29 22:31:12 +0200

    Replace ko2-reatelimit with custom code to avoid huge dependencies beeing added

--- packages/strapi-plugin-users-permissions/config/policies/rateLimit.js
+++ packages/strapi-plugin-users-permissions/config/policies/rateLimit.js
@@ -1,24 +1,78 @@
-const lazyRateLimit = {
-  get RateLimit() {
-    return require('koa2-ratelimit').RateLimit;
+const defaultOptions = {
+  interval: 60000, // 1min
+  max: 5,
+};
+
+const getMessage = ctx =>
+  ctx.request.admin
+    ? [{ messages: [{ id: 'Auth.form.error.ratelimit' }] }]
+    : 'Too many attempts, please try again in a minute.';
+
+const getKey = ctx => {
+  if (ctx.state.user && ctx.state.user.id) {
+    return `${ctx.request.url}|${ctx.state.user.id}`;
+  }
+
+  return `${ctx.request.url}|${ctx.request.ip}`;
+};
+
+const keyMap = {};
+const store = {
+  getKey(key, opts) {
+    if (!keyMap[key]) {
+      keyMap[key] = {
+        counter: 0,
+        endDate: Date.now() + opts.interval,
+      };
+    }
+    return keyMap[key];
+  },
+
+  clearExpiredKeys() {
+    const now = Date.now();
+    for (const key in keyMap) {
+      if (keyMap[key].endDate <= now) {
+        delete keyMap[key];
+      }
+    }
+  },
+
+  incr(key, opts) {
+    this.clearExpiredKeys();
+    const value = this.getKey(key, opts);
+    value.counter++;
+    return value;
+  },
+
+  decr(key, opts) {
+    const value = this.getKey(key, opts);
+    value.counter--;
+    return this;
   },
 };
 
 module.exports = async (ctx, next) => {
-  const message = ctx.request.admin
-    ? [{ messages: [{ id: 'Auth.form.error.ratelimit' }] }]
-    : 'Too many attempts, please try again in a minute.';
+  const opts = {
+    ...defaultOptions,
+    ...strapi.plugins['users-permissions'].config.ratelimit,
+  };
 
-  return lazyRateLimit.RateLimit.middleware(
-    Object.assign(
-      {},
-      {
-        interval: 1 * 60 * 1000,
-        max: 5,
-        prefixKey: `${ctx.request.url}:${ctx.request.ip}`,
-        message,
-      },
-      strapi.plugins['users-permissions'].config.ratelimit
-    )
-  )(ctx, next);
+  const key = getKey(ctx);
+
+  const { counter, endDate } = store.incr(key, opts);
+  const remaining = Math.max(opts.max - counter, 0);
+  const reset = Math.ceil(new Date(endDate).getTime() / 1000);
+
+  ctx.set('X-RateLimit-Limit', opts.max);
+  ctx.set('X-RateLimit-Remaining', remaining);
+  ctx.set('X-RateLimit-Reset', reset);
+
+  if (counter > opts.max) {
+    ctx.status = 429;
+    ctx.body = getMessage(ctx);
+    ctx.set('Retry-After', Math.ceil(opts.interval / 1000));
+    return;
+  }
+
+  return next();
 };
